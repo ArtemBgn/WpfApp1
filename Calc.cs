9bc6c58dcbe141fc1d87eecd4847a3636d19dab2
ArtemBgn <bgn.rtm@gmail.com>
2023-09-02 13:41:54 +0300

    перевірка 02.09.2023

---
+++ Calc.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace WpfApp1
+{
+    public static class Calc
+    {
+        public static decimal sum(double x, double y)
+        {
+            return (decimal)x + (decimal)y;
+        }
+        public static decimal sub(double x, double y)
+        {
+            return (decimal)x - (decimal)y;
+        }
+        public static decimal mult(double x, double y)
+        {
+            return (decimal)x * (decimal)y;
+        }
+        public static decimal div(double x, double y)
+        {
+
+            return (decimal)x / (decimal)y;
+        }
+        public static decimal remOfdiv(double x, double y)
+        {
+            return (decimal)x % (decimal)y;
+        }
+    }
+}
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,17 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
+//using Calc;
 
 namespace WpfApp1
 {
@@ -23,31 +11,64 @@ namespace WpfApp1
         public MainWindow()
         {
             InitializeComponent();
-            //MessageBox.Show("Open this one windows!");
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            int a = -1;// = Int32.Parse(textbox1.Text) + Int32.Parse(textbox2.Text);
-            switch (combobox1.SelectedIndex)
+            decimal a = -1;
+            
+            double x;
+            double y;
+            bool b1 = double.TryParse(textbox1.Text, out x);
+            bool c1 = double.TryParse(textbox2.Text, out y);
+            //MessageBox.Show($"текстбоксс 1 = {b1}\nтекстбокс 2 = {c1}");
+            //123
+
+            if (b1 && c1)
             {
-                case (0):
-                    a = Int32.Parse(textbox1.Text) + Int32.Parse(textbox2.Text);
-                    break;
-                case (1):
-                    a = Int32.Parse(textbox1.Text) - Int32.Parse(textbox2.Text);
-                    break;
-                case (2):
-                    a = Int32.Parse(textbox1.Text) * Int32.Parse(textbox2.Text);
-                    break;
-                case (3):
-                    a = Int32.Parse(textbox1.Text) / Int32.Parse(textbox2.Text);
-                    break;
-                case (4):
-                    a = Int32.Parse(textbox1.Text) % Int32.Parse(textbox2.Text);
-                    break;
+                if ((double.Parse(textbox1.Text) > double.MaxValue) || (double.Parse(textbox1.Text) < double.MinValue))
+                {
+                    MessageBox.Show("Ви ввели занадто велеке значення!\nБудь-ласка, спробуйте інше значення..");
+                    return;
+                }
+
+                if ((double.Parse(textbox2.Text) > double.MaxValue) || (double.Parse(textbox2.Text) < double.MinValue))
+                {
+                    MessageBox.Show("Ви ввели занадто мале значення!\nБудь-ласка, спробуйте інше значення..");
+                    return;
+                }
+
+                switch (combobox1.SelectedIndex)
+                {
+                    case (0):
+                        a = Calc.sum(x, y);
+                        break;
+                    case (1):
+                        a = Calc.sub(x, y);
+                        break;
+                    case (2):
+                        a = Calc.mult(x, y);
+                        break;
+                    case (3):
+                        if (y == 0)
+                        {
+                            MessageBox.Show("Ділення на 0 заборонено!\nБудь-ласка, перевірте значення!");
+                            return;
+                        }
+                        a = Calc.div(x, y);
+                        break;
+                    case (4):
+                        a = Calc.remOfdiv(x, y);
+                        break;
+                }
             }
-            label1.Content = a.ToString();
+            else
+            {
+                MessageBox.Show("Введене значення не є числом!\nБудь-ласка, перевірте введені значення!");
+                return;
+            }
+
+            label1.Content = a.ToString(format:"");
         }
     }
 }
